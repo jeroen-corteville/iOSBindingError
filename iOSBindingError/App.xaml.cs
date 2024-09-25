fc44d69b0d101e30ad3948025ac86e48c09ede64
Jeroen Corteville <jeroen.corteville@proplanner.eu>
2024-09-25 11:47:33 +0200

    Add project files.

---
+++ iOSBindingError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace iOSBindingError
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ iOSBindingError/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace iOSBindingError
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
